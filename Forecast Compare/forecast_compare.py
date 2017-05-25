634e35aa804beb564cebb98ff348656b4b6ce77d
Jacob Sword <jacobdasword@gmail.com>
2017-05-24 21:20:48 -0400

    more accurately scrape temps

--- Forecast Compare/forecast_compare.py
+++ Forecast Compare/forecast_compare.py
@@ -4,7 +4,10 @@
 import requests, bs4
 
 res = requests.get('https://weather.com/weather/tenday/l/01879')
-res.raise_for_status()
+try:
+	res.raise_for_status()
+except Exception as e:
+	print('Problem reaching site: %s' %(e))
 
 weather_page = bs4.BeautifulSoup(res.text,"html.parser")
 
@@ -12,24 +15,35 @@ day_list = []
 for day in weather_page.find_all("span", {"class" : "day-detail"}):
 	day_list.append(day.getText())
 
+#for i in day_list:
+	#print(i)
+
 temp_list = []
-for temp in weather_page.find_all("td", {"class" : "temp"}):
-	temp = list[temp.getText()]
-	temp_list.append([temp[0:1],temp[4:5]])
+for block in weather_page.find_all("td", {"class" : "temp"}):
+	for div in block:
+		for span in div.find_all("span"):
+			if span not in div.find_all("span", {"class" : "slash"}): 
+				temp = list(span.getText())
+				print(temp[0:1])
+
+#for i in temp_list:
+#	print(i)
 
 precip_list = []
 for precip in weather_page.find_all("td", {"class" : "precip"}):
 	precip_list.append(precip.getText())
 
+#for i in precip_list:
+#	print(i)
+
 weather_obj_list = []
 for i in range(15):
 	d = {
-	  'day' : day_list[i],
-	  'high' : temp_list[i][0],
-      'low' : temp_list[i][1],
-	  'precip' : precip_list[i]
+	'day' : day_list[i],
+	#'high' : temp_list[i][0],
+	#'low' : temp_list[i][1],
+	'precip' : precip_list[i]
 	}
 	weather_obj_list.append(d)
-
 for i in weather_obj_list:
 	print(i)
