9f47d67255712882907aee15efb783bcf9d9a2ed
Jacob Sword <jacobdasword@gmail.com>
2017-05-26 10:56:07 -0400

    add shelving functionality

--- Forecast Compare/forecast_compare.py
+++ Forecast Compare/forecast_compare.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #!python3
 
-import requests, bs4
+import requests, bs4, shelve, datetime
 
 res = requests.get('https://weather.com/weather/tenday/l/01879')
 try:
@@ -27,11 +27,18 @@ for block in weather_page.find_all("td", {"class" : "temp"}):
 				temp = list(span.getText())
 				first_num = str(temp[0])
 				second_num = str(temp[1])
-				first_num = first_num[1]
-				second_num = second_num[1]
+				temp_list.append(first_num+second_num)
 
-#for i in temp_list:
-#	print(i)
+#separate highs from lows in temp_list
+low_list = []
+high_list = []
+index = 0;
+for i in temp_list:
+	index += 1
+	if index % 2 == 0:
+		low_list.append(i)
+	else:
+		high_list.append(i)
 
 precip_list = []
 for precip in weather_page.find_all("td", {"class" : "precip"}):
@@ -40,14 +47,28 @@ for precip in weather_page.find_all("td", {"class" : "precip"}):
 #for i in precip_list:
 #	print(i)
 
+#construct list of weather objects
+#each weather object contains day, low, high, precip
+#list containts current day + next 15 days
 weather_obj_list = []
 for i in range(15):
 	d = {
 	'day' : day_list[i],
-	#'high' : temp_list[i][0],
-	#'low' : temp_list[i][1],
+	'low' : low_list[i],
+	'high' : high_list[i],
 	'precip' : precip_list[i]
 	}
 	weather_obj_list.append(d)
+
 #for i in weather_obj_list:
 #	print(i)
+
+#get date format for storage
+today = datetime.datetime.now()
+year = str(today.year)
+day = str(today.day)
+date = year + '-' + day
+
+#store list of weather objects in shelf file 
+weather_data_shelf = shelv.open('weather_data')
+weather_data_shelf['date'] = weather_obj_list
