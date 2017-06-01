87a58ac1bd9f0607f4f5aa2cfdcfe86f9f3e1287
Jacob Sword <jacobdasword@gmail.com>
2017-05-31 21:32:54 -0400

    create reddit sales bot

---
+++ Reddit Sale Bot/README.md
@@ -0,0 +1,2 @@
+Reddit Sale Bot uses the PRAW to monitor sale subreddits and email the user deals within the price-range they specify
+
---
+++ Reddit Sale Bot/reddit_sale_bot.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#!python3
+
+import praw, time, random, re, shelve
+
+#print("Enter reddit username")
+#username = raw_input()
+
+#print("Enter reddit password")
+#password = raw_input()
+
+#print("Enter client_id")
+#client_id = raw_input()
+
+#print("Enter secret")
+#secret = raw_input()
+
+#print("Enter desired subreddit")
+#subreddit = raw_input()
+
+#print("Enter keyword to search for")
+#keyword = raw_input()
+
+#print("Enter price upper-limit")
+#price_limit = raw_input()
+
+user_agent = ""
+r = praw.Reddit(user_agent = user_agent, client_id = "", client_secret = "OQynvMsuD-", username = "", password = "")
+
+print('\nLogging in...')
+#r.login()   #put in username and password in ext file
+
+subreddit = r.subreddit("")
+print('\nLoading subreddit...\n')
+
+if os.path.exists('Previous_Deals'):
+	previous_deals_shelf = shelve.open('Previous_Deals')
+	cache = previous_deals_shelf[cache]
+else:
+	cache = []
+
+for submission in subreddit.new(limit = "10"):
+	if keyword in submission.title:
+		#regex to find all dollar amounts < $100
+		double_digit_regex = re.compile(r'(\$( )?\d(\d)?\.(\d)?(\d)?)')
+		double_digits = double_digit_regex.search(submission.title)
+		#regex to find all dollar amounts < upper limit
+		upper_limit_regex = re.compile(r'(\$1[0-5]\d(.)?\d\d)')
+		upper_limits = upper_limit_regex.search(submission.title)
+		if str(type(double_digits)) != "<type 'NoneType'>" or str(type(upper_limits)) != "<type 'NoneType'>":
+			if submission.title not in cache:
+				#email deal
+				print(submission.title)
+				cache.append(submission.title)
+
